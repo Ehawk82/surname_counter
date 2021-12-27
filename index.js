d306ddf62e99f2cd11728641d4267246c41553b1
unknown <mrehawk82@gmail.com>
2021-12-26 20:38:40 -0500

    initial stuffs

---
+++ index.js
@@ -0,0 +1,33 @@
+import fetch from 'node-fetch';
+/*
+
+
+let input = "graziano";
+
+const response = await fetch('https://www.names.org/n/'+input+'/about#');
+const data = await response.text();
+const param_data = data.substring(
+          data.lastIndexOf("the U.S. Census Bureau surveyed") + 32,
+          data.lastIndexOf("people with the last name")
+        );
+
+const p1d = parseFloat(param_data.replace(/,/g, ''));
+
+let result = p1d;
+
+var diff = (a, b) => { return Math.abs(a - b); };
+
+let result = diff(result,result);
+if(input === ""){
+  input = "no name entered";
+}
+
+if(Number.isNaN(result)){
+   result = "there are no results for that name";
+};
+
+var r1 = (i) => {
+  console.log(i);
+}
+r1("input");
+*/
